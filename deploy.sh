b145c6f771e4de77a681078b886968a42713f28b
jelly <jellynian6@gamil.com>
2018-05-15 17:56:03 +0800

    add new post

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-rm -fr ./doc/   ./public/
+rm -fr ./docs/   ./public/
 hugo --theme=jelly
-mv ./public/   ./doc/
+mv ./public/   ./docs/
 git add --all
 git commit -m "add new post"
 git push origin master
