91c5fd0f336ae94e274492b4352526ef3caacfdc
jelly <jellynian6@gamil.com>
2018-05-25 18:51:20 +0800

    update blog by deply script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
+cd ./public
+ls  | grep -v CNAME | xargs rm -fr
+cd ..
 hugo --theme=jelly
-
+git add --all
+git commit -m "update blog by deply script"
+git push origin develop
+git subtree push --prefix=public  origin master
